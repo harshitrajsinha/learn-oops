974b88c9327c69e12c11c8ae0d360b34f5303bc3
Harshit Raj Sinha <74960032+harshitrajsinha@users.noreply.github.com>
2023-08-10 03:43:33 +0000

    unset vs destructor in PHP

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php
+/*
+# Different whitespace characters in PHP:
+
+    $firstName = "Harshit";
+    echo "Hello, World.\nWelcome\n"; //Use '\n' within strings, for MacOS use '\r'
+    echo "Your name is ".$firstName.PHP_EOL; //Use PHP_EOL anyway (it is independent of OS)
+
+    //Use < br> when inserting php code within HTML document.
+
+*/
+
 class MyClass {
-    private $changeColor = "red";
+    private $changeColor;
 
-    // public function __construct() {
-    //     echo "Object created.\n";
-    // }
-
-    public function returnColor(){
-        echo $this->changeColor."\n";
+    public function __construct($newColor) {
+        $this->changeColor = $newColor;
     }
 
-    // public function __destruct() {
-    //     echo "Object deleted.\n";
-    // }
+    public function returnColor(){
+        echo $this->changeColor.PHP_EOL;
+    }
+
+    public function __destruct() {
+        echo "Object deleted.\n".$this->changeColor.PHP_EOL;
+    }
 }
 
-$myObject = new MyClass();  // Object created.
-$myObject->returnColor();         // Object deleted (due to destruct).
-$myobject2 = clone $myObject;
+$myObject = new MyClass("green");  // Object created.
+$myObject2 = new MyClass("red");
 
-var_dump($myobject2 === $myObject)."\n";
+$myObject->returnColor();
+$myObject2->returnColor();
+
+unset($myObject2);
+sleep(2);
+
 ?>
--- php-specific.md
+++ php-specific.md
@@ -29,6 +29,9 @@ $someClassInstance->classMethod()
 An object in PHP is deleted using a combination of unset() and __destruct() [Assinging 'null' to an object just removes the reference but does not delete it]
 unset() is used to remove references to objects and make them eligible for garbage collection, __destruct() is an opportunity to perform cleanup actions before the object is actually removed from memory by the garbage collector.
 
+When we do not use either of unset() and __destruct(), object gets deleted automatically and clean up is performed and the order of deletion of object is of the order in which they were created. __destruct() is defined in order to specifically perform any further operation after deletion of object, such as to print that object has been deleted.
+unset() is used to specifically destroy any object. Use unset() when we want to destroy any object in-between the code and not at last OR use in order to change the order of object destruction. For example - if $obj1 and $obj2 are created resp. and use of unset($obj2) will instruct the program to destroy and perform clean up for $obj2 first and then at the end of program when $obj1 is not reachable, automatically destroy $obj1 and perform cleanup.
+
 # Cloning vs Assigning an object to an instantiated object
 
 <!-- <?php
